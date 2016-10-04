68abd57a32ca138535386e629a747c4b797746aa
jshalamanoski <jovsal@outlook.com>
2016-10-04 15:05:22 +0200

    added application.properties

--- src/main/java/MovieRepository.java
+++ src/main/java/MovieRepository.java
@@ -1,13 +1,18 @@
+import org.omg.CORBA.Environment;
 import org.omg.CORBA.portable.InputStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class MovieRepository {
+
+//    @Value("${fileLocation}")
+//    private String filePath;
 
     public void read(){
         System.out.println("read movie");
@@ -32,11 +37,6 @@ public class MovieRepository {
     }
 
     public void insert(Movie m) throws IOException {
-//        BufferedWriter bufferedWriter = new BufferedWriter(
-//                new FileWriter(file.getAbsoluteFile()));
-//        System.out.println(line);
-//        bufferedWriter.write(line);
-//        bufferedWriter.close();
         File file = new File("C:\\gitProjects\\spring-core-practice\\src\\main\\resources\\movies.txt");
         String line = m.getName()+ "," +m.getYearReleased();
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file, true)));
